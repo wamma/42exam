2212b59ec718183526e768ef326e48584c61b33f
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-05-07 11:56:21 +0900

    Fix: rank05

--- rank05/cpp_module00/Warlock.hpp
+++ rank05/cpp_module00/Warlock.hpp
@@ -2,6 +2,8 @@
 
 #include "ASpell.hpp"
 #include <iostream>
+#include <map>
+#include <string>
 
 class ASpell;
 
@@ -10,6 +12,7 @@ class Warlock
   private:
     std::string name;
     std::string title;
+    std::map<std::string, ASpell *> spellBook;
 
     Warlock();
     Warlock &operator=(const Warlock &other);
@@ -23,4 +26,8 @@ class Warlock
     const std::string &getTitle() const;
     void setTitle(const std::string &title);
     void introduce() const;
+
+    void learnSpell(ASpell *spell);
+    void forgetSpell(const std::string &spellName);
+    void launchSpell(const std::string &spellName, const ATarget &target);
 };
